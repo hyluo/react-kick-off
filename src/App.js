169c7cb595954ad8e9dbca2fc6992dfbe7297f22
hyluo <maplein42@gmail.com>
2018-05-03 20:25:11 +0800

    finish ticket component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Tickers from './components/Tickers.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <h2>Cryptocurrency Ticker</h2>
-        </header>
+        </div>
+        <Tickers />
       </div>
     );
   }
