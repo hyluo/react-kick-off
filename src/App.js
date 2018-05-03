549ce765e47f6de2cadd363142a818f16be070d7
hyluo <maplein42@gmail.com>
2018-05-03 20:19:13 +0800

    init sabe.io

--- src/App.js
+++ src/App.js
@@ -7,15 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2>Cryptocurrency Ticker</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
